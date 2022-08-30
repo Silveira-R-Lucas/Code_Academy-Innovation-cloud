c9a50b40dd0f00b2282fd8693965e25f1662f678
Lucas Silveira <silveira.codes@gmail.com>
2022-08-30 18:40:48 -0300

    Atualização dos arquivos

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root 'signups#new'
+
+  get '/thanks' => 'pages#thanks'
+
+  resources :signups
+  
+  post 'signups' => 'signups#create'
+end
